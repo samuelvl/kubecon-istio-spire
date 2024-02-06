042d86acf4b2789b98994ae784f4305e800f37ca
Samu Veloso <veloso.lopez.samuel@gmail.com>
2024-02-06 14:16:37 +0100

    install spire quickstart

--- scripts/lib/spire.sh
+++ scripts/lib/spire.sh
@@ -1,12 +1,45 @@
 #!/bin/sh
-set -o nounset
+set -u -o errexit -x
 
 SPIRE_NAMESPACE="spire"
+SPIRE_MANIFESTS_URL="https://raw.githubusercontent.com/spiffe/spire-tutorials/main/k8s/quickstart"
 
 spire_install() {
-    context="${1}"
-    kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" \
-        -f https://raw.githubusercontent.com/istio/istio/release-1.19/samples/security/spire/spire-quickstart.yaml
+  clusters_contexts="${1}"
+  for context in ${clusters_contexts}; do
+    spire_install_server "${context}"
+    spire_install_agent "${context}"
+    spire_wait_for_node_attestation "${context}"
+  done
+}
+
+spire_install_server() {
+  context="${1}"
+  kubectl create --context="${context}" namespace "${SPIRE_NAMESPACE}" || true
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/server-account.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/server-cluster-role.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/server-configmap.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/spire-bundle-configmap.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/server-service.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/server-statefulset.yaml"
+}
+
+spire_install_agent() {
+  context="${1}"
+  kubectl create --context="${context}" namespace "${SPIRE_NAMESPACE}" || true
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/agent-account.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/agent-cluster-role.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/agent-configmap.yaml"
+  kubectl apply --context="${context}" -n "${SPIRE_NAMESPACE}" -f "${SPIRE_MANIFESTS_URL}/agent-daemonset.yaml"
+}
+
+spire_wait_for_node_attestation() {
+  context="${1}"
+  for _ in $(seq 60); do
+    if (kubectl logs --context="${context}" -n "${SPIRE_NAMESPACE}" --tail=-1 pod/spire-server-0 |
+      grep -e ".*Agent attestation request completed.*k8s_sat.*"); then break; fi
+    sleep 1
+  done
 }
 
 "$@"
